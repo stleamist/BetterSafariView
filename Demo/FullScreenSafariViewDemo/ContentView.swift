cb582e4187ad8b81d6d3bdb76870a68737528798
Dongkyu Kim <stleamist@me.com>
2020-05-15 12:04:39 +0900

    Add a demo project

---
+++ Demo/FullScreenSafariViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FullScreenSafariViewDemo
+//
+//  Created by 김동규 on 2020/05/15.
+//  Copyright © 2020 Stleam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
