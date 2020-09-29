91fafa3d9bb4477833b165f14b89d4e6bda3d724
Dongkyu Kim <stleamist@me.com>
2020-09-30 08:46:37 +0900

    Add macOS support to WebAuthenticationSession

--- Sources/BetterSafariView/WebAuthenticationSession/WebAuthenticationPresenter.swift
+++ Sources/BetterSafariView/WebAuthenticationSession/WebAuthenticationPresenter.swift
@@ -2,30 +2,63 @@ import SwiftUI
 import SafariServices
 import AuthenticationServices
 
-struct WebAuthenticationPresenter<Item: Identifiable>: UIViewControllerRepresentable {
+#if os(iOS)
+typealias ViewController = UIViewController
+typealias ViewControllerRepresentable = UIViewControllerRepresentable
+#elseif os(macOS)
+typealias ViewController = NSViewController
+typealias ViewControllerRepresentable = NSViewControllerRepresentable
+#endif
+
+struct WebAuthenticationPresenter<Item: Identifiable>: ViewControllerRepresentable {
     
     // MARK: Representation
     
     @Binding var item: Item?
     var representationBuilder: (Item) -> WebAuthenticationSession
     
-    // MARK: UIViewControllerRepresentable
+    // MARK: ViewControllerRepresentable
     
     func makeCoordinator() -> Coordinator {
         return Coordinator(parent: self)
     }
     
+    #if os(iOS)
+    
     func makeUIViewController(context: Context) -> UIViewController {
-        return context.coordinator.uiViewController
+        return makeViewController(context: context)
     }
     
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         
+        updateViewController(uiViewController, context: context)
+        
         // To set a delegate for the presentation controller of an `SFAuthenticationViewController` as soon as possible,
         // check the view controller presented by `uiViewController` then set it as a delegate on every view updates.
         // INFO: `SFAuthenticationViewController` is a private subclass of `SFSafariViewController`.
-        context.coordinator.setInteractiveDismissalDelegateIfPossible()
-        
+        guard #available(iOS 14.0, *) else {
+            context.coordinator.setInteractiveDismissalDelegateIfPossible()
+            return
+        }
+    }
+    
+    #elseif os(macOS)
+    
+    func makeNSViewController(context: Context) -> NSViewController {
+        return makeViewController(context: context)
+    }
+    
+    func updateNSViewController(_ nsViewController: NSViewController, context: Context) {
+        updateViewController(nsViewController, context: context)
+    }
+    
+    #endif
+    
+    private func makeViewController(context: Context) -> ViewController {
+        return context.coordinator.viewController
+    }
+    
+    private func updateViewController(_ viewController: ViewController, context: Context) {
         // Keep the coordinator updated with a new presenter struct.
         context.coordinator.parent = self
         context.coordinator.item = item
@@ -34,7 +67,7 @@ struct WebAuthenticationPresenter<Item: Identifiable>: UIViewControllerRepresent
 
 extension WebAuthenticationPresenter {
     
-    class Coordinator: NSObject, ASWebAuthenticationPresentationContextProviding, UIAdaptivePresentationControllerDelegate {
+    class Coordinator: NSObject, ASWebAuthenticationPresentationContextProviding {
         
         // MARK: Parent Copying
         
@@ -46,7 +79,7 @@ extension WebAuthenticationPresenter {
         
         // MARK: View Controller Holding
         
-        let uiViewController = UIViewController()
+        let viewController = ViewController()
         private var session: ASWebAuthenticationSession?
         
         // MARK: Item Handling
@@ -109,25 +142,45 @@ extension WebAuthenticationPresenter {
         // by calling `present(_:animated:completion:)` method from a root view controller of the window.
         
         func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
-            return uiViewController.view.window!
+            return viewController.view.window!
         }
         
-        // MARK: UIAdaptivePresentationControllerDelegate
+        #if os(iOS)
+        
+        // MARK: InteractiveDismissalDelegate
         
         // There is a problem that `item` is not set to `nil` after the sheet is dismissed with pulling down
-        // because the completion handler is not called on this case due to a system bug.
-        // To resolve this issue, set `Coordinator` as a presentation controller delegate of `SFAuthenticationViewController`
+        // because the completion handler is not called on this case due to a system bug on iOS 13.
+        // To resolve this issue, set `interactiveDismissalDelegate` as a presentation controller delegate of `SFAuthenticationViewController`
         // so that ensures the completion handler is always called.
         
+        @available(iOS, introduced: 13.0, deprecated: 14.0)
+        private lazy var interactiveDismissalDelegate = InteractiveDismissalDelegate(coordinator: self)
+        
+        @available(iOS, introduced: 13.0, deprecated: 14.0)
         func setInteractiveDismissalDelegateIfPossible() {
-            guard let safariViewController = uiViewController.presentedViewController as? SFSafariViewController else {
+            guard let safariViewController = viewController.presentedViewController as? SFSafariViewController else {
                 return
             }
-            safariViewController.presentationController?.delegate = self
+            safariViewController.presentationController?.delegate = interactiveDismissalDelegate
         }
         
-        func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
-            resetItemBinding()
+        @available(iOS, introduced: 13.0, deprecated: 14.0)
+        class InteractiveDismissalDelegate: NSObject, UIAdaptivePresentationControllerDelegate {
+            
+            weak var coordinator: WebAuthenticationPresenter.Coordinator?
+            
+            init(coordinator: WebAuthenticationPresenter.Coordinator) {
+                self.coordinator = coordinator
+            }
+            
+            // MARK: UIAdaptivePresentationControllerDelegate
+            
+            func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+                coordinator?.resetItemBinding()
+            }
         }
+        
+        #endif
     }
 }
