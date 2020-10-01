817eee98e21100bc276dc6470b1a0ed5098b0371
Dongkyu Kim <stleamist@me.com>
2020-10-01 19:10:22 +0900

    Add RootView.swift

---
+++ Demo/watchOS Extension/RootView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
