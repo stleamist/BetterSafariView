f4ffc77a706548abc1766c75603a943e41fe62a3
Dongkyu Kim <stleamist@me.com>
2020-10-01 18:11:36 +0900

    Add watchOS target

---
+++ Demo/BetterSafariViewDemo WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterSafariViewDemo WatchKit Extension
+//
+//  Created by 김동규 on 2020/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
