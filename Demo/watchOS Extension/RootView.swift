55c5c784bf65556d9a3fff7e0ef6bd2a8ef7c676
Dongkyu Kim <stleamist@me.com>
2020-10-02 11:18:41 +0900

    Add contents to RootView.swift

--- Demo/watchOS Extension/RootView.swift
+++ Demo/watchOS Extension/RootView.swift
@@ -1,8 +1,47 @@
 import SwiftUI
+import BetterSafariView
 
 struct RootView: View {
+    
+    @State private var webAuthenticationSessionOptions = WebAuthenticationSessionOptions()
+    @State private var showingWebAuthenticationSession = false
+    @State private var showingWebAuthenticationSessionOptionsForm = false
+    @State private var webAuthenticationSessionCallbackURL: URL? = nil
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                Section(header: Text("WebAuthenticationSession").textCase(nil)) {
+                    Button(action: { showingWebAuthenticationSession = true }) {
+                        Text("Start Session")
+                    }
+                    .webAuthenticationSession(isPresented: $showingWebAuthenticationSession) {
+                        WebAuthenticationSession(
+                            url: webAuthenticationSessionOptions.url!,
+                            callbackURLScheme: webAuthenticationSessionOptions.callbackURLScheme
+                        ) { callbackURL, error in
+                            webAuthenticationSessionCallbackURL = callbackURL
+                        }
+                        .prefersEphemeralWebBrowserSession(webAuthenticationSessionOptions.prefersEphemeralWebBrowserSession)
+                    }
+                    .alert(item: $webAuthenticationSessionCallbackURL) { callbackURL in
+                        Alert(
+                            title: Text("Session Completed with Callback URL"),
+                            message: Text(callbackURL.absoluteString),
+                            dismissButton: nil
+                        )
+                    }
+                    
+                    Button(action: { showingWebAuthenticationSessionOptionsForm = true }) {
+                        Text("Options")
+                    }
+                    .sheet(isPresented: $showingWebAuthenticationSessionOptionsForm) {
+                        WebAuthenticationSessionOptionsForm(options: $webAuthenticationSessionOptions)
+                    }
+                }
+            }
+            .navigationTitle(Text("BetterSafari"))
+        }
     }
 }
 
