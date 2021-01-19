462e76624c4e4f0f8cfb8b85cc71fd9bf22da652
Dongkyu Kim <stleamist@me.com>
2021-01-20 00:18:54 +0900

    Add a summary comment to UIView+viewController extension

--- Sources/BetterSafariView/UIView+viewController.swift
+++ Sources/BetterSafariView/UIView+viewController.swift
@@ -3,6 +3,10 @@
 import UIKit
 
 extension UIView {
+    
+    /// The receiver’s view controller, or `nil` if it has none.
+    ///
+    /// This property is `nil` if the view has not yet been added to a view controller.
     var viewController: UIViewController? {
         if let nextResponder = self.next as? UIViewController {
             return nextResponder
